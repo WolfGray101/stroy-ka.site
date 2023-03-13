2405c4afb99392a88ea528f39eb3679da990813a
wolfgray <5565486@mail.ru>
2023-03-13 22:54:39 +0300

    add prj

---
+++ js.js
@@ -0,0 +1,27 @@
+const modalMenu = document.querySelector(".modal");
+const burger = document.querySelector(".burger-container");
+const closeBtn = document.querySelector(".close");
+const main = document.querySelector("main");
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn')
+
+burger.addEventListener("click", () => {
+  const height = modalMenu.offsetHeight;
+  modalMenu.classList.add("modal-menu__open");
+  closeBtn.classList.add('close__open')
+  main.style.height = `${height}px`;
+  main.classList.add('main__open');
+});
+
+const removeClassList = () => {
+  modalMenu.classList.remove("modal-menu__open");
+  main.style.height = `auto`;
+  closeBtn.classList.remove('close__open')
+  main.classList.remove('main__open');
+
+}
+closeBtn.addEventListener("click", () => {
+  removeClassList()
+});
+mobileMenuBtn.addEventListener("click", () => {
+  removeClassList()
+});
