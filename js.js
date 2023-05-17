ad33eaadb97ccc17c929c4a64342377c40742504
wolfgray <5565486@mail.ru>
2023-05-17 12:51:15 +0300

    redesign

--- js.js
+++ js.js
@@ -5,18 +5,18 @@ const main = document.querySelector("main");
 const mobileMenuBtn = document.querySelector('.mobile-menu-btn')
 
 burger.addEventListener("click", () => {
-  const height = modalMenu.offsetHeight;
+  // const height = modalMenu.offsetHeight;
   modalMenu.classList.add("modal-menu__open");
   closeBtn.classList.add('close__open')
-  main.style.height = `${height}px`;
-  main.classList.add('main__open');
+  // main.style.height = `${height}px`;
+  // main.classList.add('main__open');
 });
 
 const removeClassList = () => {
   modalMenu.classList.remove("modal-menu__open");
-  main.style.height = `auto`;
+  // main.style.height = `auto`;
   closeBtn.classList.remove('close__open')
-  main.classList.remove('main__open');
+  // main.classList.remove('main__open');
 
 }
 closeBtn.addEventListener("click", () => {
